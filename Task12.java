5e5ea440487bcc0e3550673a82722482e4eb3291
lsvphanidhar31 <lsvphanidhar31@gmail.com>
2026-08-20 09:02:09 +0530

    Create Task12.java

---
+++ Task12.java
@@ -0,0 +1,33 @@
+class Task12 {
+    public boolean isPalindrome(ListNode head) {
+          ListNode slow = head;
+          ListNode fast  = head;
+          while(fast!=null && fast.next!=null){
+               slow = slow.next;
+               fast = fast.next.next;
+          }
+
+          ListNode prev = null;
+          ListNode curr = slow;
+          
+          while(curr!=null){
+            ListNode next = curr.next;
+          curr.next = prev;
+          prev = curr;
+          curr = next;
+          }
+
+          ListNode left = head;
+          ListNode right = prev;
+
+          while(right!=null){
+            if(left.val != right.val){
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+          }
+         return true;
+          
+    }
+}
