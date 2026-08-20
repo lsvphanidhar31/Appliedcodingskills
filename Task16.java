e953ad4e2b9d8b68d0d8e193fd3ffe23e09ff8a0
lsvphanidhar31 <lsvphanidhar31@gmail.com>
2026-08-20 09:07:18 +0530

    Create Task16.java

---
+++ Task16.java
@@ -0,0 +1,33 @@
+
+class Task16 {
+    public ListNode reverseKGroup(ListNode head, int k) {
+            ListNode temp = head;
+            int count =0;
+
+            while(count <k ){
+                if(temp == null){
+                    return head;
+                }
+                temp = temp.next;
+                count++;
+            }
+
+            ListNode prevNode = reverseKGroup(temp, k);
+
+            temp = head;
+            count =0;
+
+            while(count<k){
+                ListNode next =temp.next;
+                temp.next = prevNode;
+                prevNode = temp;
+                temp = next;
+
+
+
+                count++;
+            }
+
+            return prevNode;
+    }
+}
